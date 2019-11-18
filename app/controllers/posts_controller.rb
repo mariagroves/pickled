ea00c1dca894a34b82effb4d22d9f9e78d41ba72
sanjana.anantbhat178@gmail.com <sanjana.anantbhat178@gmail.com>
2019-11-18 16:02:26 +0100

    Done with adding controller to post

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,48 @@
+class PostsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  def delete
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:text, :description)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,50 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Post.destroy_all
+User.destroy_all
+
+puts "Creating users..."
+
+user1 = User.new(
+  email: "user1@lewagon.com",
+  password: "123456",
+  username: "user1",
+  bio: "I am cool"
+)
+
+user1.save!
+
+user2 = User.new(
+  email: "user2@lewagon.com",
+  password: "123456",
+  username: "user2",
+  bio: "Nice and happy"
+)
+
+user2.save!
+
+puts "Finished creating users"
+
+puts "Creating posts..."
+
+
+post1 = Post.new(
+  title: "In a fix need help",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere!",
+)
+
+post2 = Post.new(
+  title: "Help me!!!!",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere!",
+)
+
+post1.user = user2
+post2.user = user1
+
+post1.save!
+post2.save!
+
+puts "Finished creating posts"
+
+
