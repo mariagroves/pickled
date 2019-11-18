c317b2cac136cd9c224e4169510fa705af17f79b
Maria Groves <mariagrovesh@gmail.com>
2019-11-18 16:08:04 +0100

    made controller for post options

---
+++ app/controllers/post_options_controller.rb
@@ -0,0 +1,18 @@
+class PostOptionsController < ApplicationController
+  def new
+    @option = PostOption.new
+  end
+
+  def create
+    @option = PostOption.new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
---
+++ test/controllers/post_options_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PostOptionsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
