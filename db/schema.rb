d433a783aa0c525d7a58a63debc3880a2f29fffd
Maria Groves <mariagrovesh@gmail.com>
2019-11-19 14:08:40 +0100

    added kind to posts table and changed validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_11_19_110838) do
+ActiveRecord::Schema.define(version: 2019_11_19_130548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +54,7 @@ ActiveRecord::Schema.define(version: 2019_11_19_110838) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "kind"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
