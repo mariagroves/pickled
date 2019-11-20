3864a5352a05f0a1d865fdd7393a01a6526b4ff3
Carla Paloma <learntoglow@gmail.com>
2019-11-20 13:45:08 +0100

    voting cards

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :post_options, dependent: :destroy
+  has_many :post_votes, through: :post_options
   has_many :post_categories
   has_many :comments
   accepts_nested_attributes_for :post_options
