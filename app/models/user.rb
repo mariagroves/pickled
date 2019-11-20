edc422f45976eab7d671029cfe00a15c71785ac6
Maria Groves <mariagrovesh@gmail.com>
2019-11-20 11:43:51 +0100

    added a method to the user model to count votes

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def vote_count
+    post_votes.count
+  end
 end
