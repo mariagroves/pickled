3130b392316e312deaed76252e625f624aa5bc26
Maria Groves <mariagrovesh@gmail.com>
2019-11-21 11:20:53 +0100

    Revert "added category filtering using buttons on the index page"
    This reverts commit 95d6542dbb3e2cda73079ab914ce7c5fb79d5799.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
-    @categories = Category.all
     @posts = Post.all
-    @posts = @posts.joins(:post_categories).where(post_categories: { category_id: params[:category]}) if params[:category]
   end
 
   def show
