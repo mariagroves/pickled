e35f1d2473d0494435dd7c5b18a71947ea74040a
sanjana.anantbhat178@gmail.com <sanjana.anantbhat178@gmail.com>
2019-11-21 17:52:04 +0100

    Completed the categorybar in the index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,8 +26,8 @@ class PostsController < ApplicationController
     @post.user = current_user
 
     if @post.save
-      categoy_ids = params[:post][:category_ids]
-      categoy_ids.each do |category_id|
+      category_ids = params[:post][:category_ids]
+      category_ids.each do |category_id|
         PostCategory.create(post: @post, category_id: category_id)
       end
       redirect_to post_path(@post)
@@ -40,6 +40,10 @@ class PostsController < ApplicationController
   end
 
   def update
+    category_ids = Category.where id: post_params[:category_ids]
+    category_ids.each do |category_id|
+      PostCategory.create(post: @post, category_id: category_id)
+    end
     if @post.update(post_params)
       redirect_to post_path(@post)
     else
@@ -60,6 +64,6 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:title, :description, :kind,
-      post_options_attributes: [:id, :image, :content])
+      post_options_attributes: [:id, :image, :content], category_ids: [])
   end
 end
