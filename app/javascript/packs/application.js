b77edec0dce5674ce570122093d35aaa3d59a6f8
sanjana.anantbhat178@gmail.com <sanjana.anantbhat178@gmail.com>
2019-11-22 15:26:22 +0100

    Styled the checkboxes of the category

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,10 @@
 import "bootstrap";
 
 require("components/form")
+
+$(document).ready(function(){
+  $(".category-choice").click(function(){
+    console.log("hey")
+    $(this).toggleClass("active");
+  });
+});
