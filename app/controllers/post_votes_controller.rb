e7ced5fe39b853365742f5eca76e365f226c7266
Maria Groves <mariagrovesh@gmail.com>
2019-11-25 15:05:56 +0100

    adding ajax to voting on index page

--- app/controllers/post_votes_controller.rb
+++ app/controllers/post_votes_controller.rb
@@ -4,17 +4,18 @@ class PostVotesController < ApplicationController
     @post_vote = PostVote.new(post_vote_params)
     @post = Post.find(params[:post_id])
     @post_vote.user = current_user
-    if @post_vote.save
-      redirect_to post_path(@post)
-    else
-      errors_messages = @post_vote.errors.messages
-      if errors_messages[:post_option].present?
-        flash[:alert] = errors_messages[:post_option].first
-      else
-        flash[:alert] = "Something went wrong."
-      end
-      render 'posts/show'
-    end
+    # if
+      @post_vote.save
+      # redirect_to post_path(@post)
+    # else
+      # errors_messages = @post_vote.errors.messages
+      # if errors_messages[:post_option].present?
+      #   flash[:alert] = errors_messages[:post_option].first
+      # else
+      #   flash[:alert] = "Something went wrong."
+      # end
+      # render 'posts/show'
+    # end
   end
 
   def post_vote_params
