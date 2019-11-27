5916476383d5ff1143bd1a79a1c9145a08276942
Carla Paloma <learntoglow@gmail.com>
2019-11-27 12:01:48 +0100

    category background colors matches

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts do
     resources :comments, only: [ :create, :destroy ]
