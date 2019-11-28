8cd497de1c23cda566b93a350fa81e57e0cc1066
Maria Groves <mariagrovesh@gmail.com>
2019-11-28 10:27:34 +0100

    added validation

--- app/models/post_vote.rb
+++ app/models/post_vote.rb
@@ -13,11 +13,15 @@ class PostVote < ApplicationRecord
   belongs_to :user
   belongs_to :post_option
   validate :one_vote_per_post
+  validate :vote_own_post
 
   def one_vote_per_post
     votes = user.own_votes.joins(:post_option).where('post_options.post_id = ?', post_option.post.id)
     # votes = user.own_votes.joins(:post_option).where(post_options: { post_id: self.post_option.post.id })
     self.errors.add(:post_option, "You have already voted!") unless votes.empty?
   end
+
+  def vote_own_post
+    self.errors.add(:post_option, "You can't vote for your own post!") if self.user == self.post_option.post.user
+  end
 end
-#
